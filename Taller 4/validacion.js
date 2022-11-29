caf72e11b2ee7eedb40e85be0cd88887dec35ff2
LRC25 <laura2504rc@gmail.com>
2022-11-28 21:18:35 -0500

    correcciones

--- Taller 4/validacion.js
+++ Taller 4/validacion.js
@@ -5,11 +5,11 @@ const message = document.querySelectorAll("#form h6");
 
 //Se utilizan algunas expresiones regulares para verificar los campos
 const expresiones = {
-	nombre:/^[a-zA-Z0-9\_\-]{1,25}$/,
-    apellido:/^[a-zA-Z0-9\_\-]{1,25}$/,
-    direccion:/^(cra|cll|av|anv|trans)(\s)?([a-zA-Z]{0,15}|[0-9]{1,3})(\s)?[a-zA-Z]?(\s)?(Este|Norte|Occidente|Oeste|Sur)?(\s)?([a-zA-Z]{0,15}|[0-9]{1,3})?(\s)?(#(\s)?[0-9]{1,2}(\s)?[a-zA-Z]?(\s)?(Este|Norte|Occidente|Oeste|Sur)?(\s)?(-)?(\s)?(Este|Norte|Occidente|Oeste|Sur)?)?((\s)?[1-9][0-9]{0,3})*$/   ,
-    usuario:/\w{10,20}/,
-    contraseña:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*.#&])[A-Za-z\d@$!%*?&]{10,20}$/im,
+	direccion: /^(cll|cra|av|anv|trans)/, // Comienza con cll, cra, av, anv o trans
+	ccusuario: /^\d{10,20}$/, // El número de identificación debe tener entre 10 a 20 caracteres y no contener caracteres extraños (solo números)
+    icon: /^(b|incorrect)/,
+	message:  /^m/,
+    contraseña:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#&])[A-Za-z\d@$!#%*?&]{10,20}$/,
     email:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     telefono:/^[0-9]{7,10}$/
 }
@@ -113,52 +113,73 @@ const validarFormulario = (e) => {
 			} 
 			
 			else{
+				document.getElementById("m7").style.display="none";
+				document.getElementById("m6").style.display="block";
+				document.getElementById("b4").style.display="none";
+				document.getElementById("incorrect4").style.display="block";
+				campos.ccusuario = false;
 				
 			}
 		break;
 		case "password":
-			;
+			const password = document.getElementById('password');
+            const passwordV = password.value.trim();
+			if(expresiones.contraseña.test(passwordV) ){
+				document.getElementById("m8").style.display="none";
+				document.getElementById("b5").style.display="block";
+				document.getElementById("incorrect5").style.display="none";
+			}
+			
+			else{
+				document.getElementById("m8").style.display="block";
+				document.getElementById("b5").style.display="none";
+				document.getElementById("incorrect5").style.display="block";
+			}
 		break;
-        case "password2":
-	function verificar_pass()
- {
-    var c1 = document.getElementById('password').value
-    var c2 = document.getElementById('password2').value
+        case "password2": verificar_pass()
 
-    if (c1 != c2){
-        document.getElementById("m7").style.display="none";
-	document.getElementById("m8").style.display="none";
-	document.getElementById("b6").style.display="block";
-	document.getElementById("incorrect6").style.display="none";
-	campos.password = true;
-
-    } else {
-        document.getElementById("m7").style.display="none";
-	document.getElementById("m8").style.display="block";
-	document.getElementById("b6").style.display="none";
-	document.getElementById("incorrect6").style.display="block";
-	campos.passsword = false;
-    }
-}
 		break;
         case "email":
-				const direccion = document.getElementById('email');
-            const emailV = email.value.trim();
+				const email = document.getElementById('email');
+                const emailV = email.value.trim();
 			if(expresiones.email.test(emailV) ){
-				document.getElementById("m9").style.display="none";
+				document.getElementById("m10").style.display="none";
 				document.getElementById("b7").style.display="block";
 				document.getElementById("incorrect7").style.display="none";
 			}
 			
 			else{
-				document.getElementById("m9").style.display="block";
+				document.getElementById("m10").style.display="block";
 				document.getElementById("b7").style.display="none";
 				document.getElementById("incorrect7").style.display="block";
 			};
 		break;
+
+		case "telefono": 
+		   document.getElementById("b8").style.display="block";
+		break;
 	}
 }
 
+
+function verificar_pass()
+{
+   var c1 = document.getElementById('password').value
+   var c2 = document.getElementById('password2').value
+
+   if (c1 == c2){
+	   document.getElementById("m9").style.display="none";
+	   document.getElementById("b6").style.display="block";
+	   document.getElementById("incorrect6").style.display="none";
+	   campos.password = true;
+
+   } else {
+	   document.getElementById("m9").style.display="block";
+	   document.getElementById("b6").style.display="none";
+	   document.getElementById("incorrect6").style.display="block";
+	   campos.password = false;
+   }
+}
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario);
@@ -170,7 +191,7 @@ inputs.forEach((input) => {
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	
-	if(campos.nombre && campos.apellido && campos.ccusuario){
+	if(campos.nombre && campos.apellido && campos.ccusuario && campos.password && campos.email ){
 	   alert ("Se enviará el formulario") 
 	}
 
@@ -194,3 +215,12 @@ form.addEventListener("reset", (e) => {
 });
 
 
+//Habilitar registro de gusto de usuarios
+function mostrar(){
+	document.getElementById("f1").style.display="block"  ;
+  };
+
+//Deshabilitar registro de gusto de usuarios
+  function ocultar(){
+    document.getElementById("f1").style.display="none"  ;
+};

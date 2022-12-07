697cebc104fbdf441129c0cfcbcfcff6d28e329b
Skarterhg <106789623+Skarterhg@users.noreply.github.com>
2022-12-07 13:42:13 -0500

    Update validacion.js

--- Taller 5/validacion.js
+++ Taller 5/validacion.js
@@ -19,7 +19,40 @@ const campos = {
     email: false,
     ccusuario: false,
 }
+const fechaNacimiento = document.getElementById("fecha");
+const edad = document.getElementById("edad");
 
+const calcularEdad = (fechaNacimiento) => {
+    const fechaActual = new Date();
+    const anoActual = parseInt(fechaActual.getFullYear());
+    const mesActual = parseInt(fechaActual.getMonth()) + 1;
+    const diaActual = parseInt(fechaActual.getDate());
+
+    // 2016-07-11
+    const anoNacimiento = parseInt(String(fechaNacimiento).substring(0, 4));
+    const mesNacimiento = parseInt(String(fechaNacimiento).substring(5, 7));
+    const diaNacimiento = parseInt(String(fechaNacimiento).substring(8, 10));
+
+    let edad = anoActual - anoNacimiento;
+    if (mesActual < mesNacimiento) {
+        edad--;
+    } else if (mesActual === mesNacimiento) {
+        if (diaActual < diaNacimiento) {
+            edad--;
+        }
+    }
+    return edad;
+};
+
+window.addEventListener('load', function () {
+
+    fechaNacimiento.addEventListener('change', function () {
+        if (this.value) {
+            edad.innerText = ` Tu edad es: ${calcularEdad(this.value)} años`;
+        }
+    });
+
+});
 
 
 const validarFormulario = (e) => {
